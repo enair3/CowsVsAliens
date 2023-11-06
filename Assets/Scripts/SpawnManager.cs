eaf7abc7c94592b5ebcc2991949e6043fc5d3c90
enair3 <91293037+enair3@users.noreply.github.com>
2023-11-05 19:03:00 -0600

    toggle attempt

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,22 +7,38 @@ public class SpawnManager : MonoBehaviour
     public static SpawnManager spawnManager;
 
     // spawning vars
-    public int mode;
-    public int blocksSpawned;
+    public int totalBlocks;
+
+    private SpawnThisSingle spawnThisSingle;
+    private SpawnThisDouble spawnThisDouble;
 
     public bool singleOn;
-    public bool doubleOff;
+    public bool doubleOn;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //this.GetComponent<SpawnThisSingle>.enabled = true;
+        spawnThisSingle = GetComponent<SpawnThisSingle>();
+        spawnThisDouble = GetComponent<SpawnThisDouble>();
+
+        spawnThisSingle.enabled = true;
+        spawnThisDouble.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (singleOn)
+        {
+            spawnThisSingle.enabled = true;
+            spawnThisDouble.enabled = false;
+        }
+
+        if (doubleOn)
+        {
+            spawnThisSingle.enabled = false;
+            spawnThisDouble.enabled = true;
+        }
     }
 }
