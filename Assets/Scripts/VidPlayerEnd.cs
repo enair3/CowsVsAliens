6c29d6d69461d48803916c686795d27478f73ff0
cwc-3000 <cwc3000@utexas.edu>
2023-12-03 00:02:15 -0600

    added both game over cutscenes, and buttons that go to the game over screen; also added obstacle ufo-explosion animation prefab

---
+++ Assets/Scripts/VidPlayerEnd.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VidPlayerEnd : MonoBehaviour
+{
+	
+	[SerializeField] string videoFileName;
+    public VideoPlayer vidPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayVideo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayVideo()
+    {
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+
+        if (videoPlayer)
+        {
+            string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
+            Debug.Log(videoPath);
+            videoPlayer.url = videoPath;
+            videoPlayer.Play();
+        }
+    }
+}
