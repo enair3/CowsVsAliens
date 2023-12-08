4467dc90cdb39b3233e0da54ea551d909e9c4b08
enair3 <91293037+enair3@users.noreply.github.com>
2023-12-08 14:49:05 -0600

    names after first score bug fixed, fbi penalty values changed

--- Assets/Scripts/NameText.cs
+++ Assets/Scripts/NameText.cs
@@ -6,10 +6,28 @@ using TMPro;
 public class NameText : MonoBehaviour
 {
     public TextMeshProUGUI nameUI;
+    public static NameText nameText;
+    public static NameText Instance;
+
+    private void Start()
+    {
+        nameText = this;
+        DontDestroyOnLoad(nameText);
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Update()
     {
 
         //nameUI.text = string.Format(SceneManagement.sceneManager.names[AlienNames.alienNames.alienNameIndex - 1]);
-        nameUI.text = SceneManagement.sceneManager.entryName;
+        nameUI.text = SceneManagement.Instance.entryName;
     }
 }
--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -15,7 +15,7 @@ public class SceneManagement : MonoBehaviour
                                                             "g", "h", "i", "j", "k", "l",
                                                             "m", "n", "o", "p", "q", "r",
                                                             "s", "t", "u", "v", "w", "x", "y", "z"};
-    public string entryName;
+    public string entryName = new string(" ");
 
     private void Awake()
     {
