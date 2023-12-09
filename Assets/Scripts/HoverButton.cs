eb019202e47dd150805124ac15ef4a3f6a95f3aa
enair3 <91293037+enair3@users.noreply.github.com>
2023-12-08 19:08:52 -0600

    switched back to windows build
    SWITCHED TO ALT CTRL IN HOVERBUTTON SCRIPT

--- Assets/Scripts/HoverButton.cs
+++ Assets/Scripts/HoverButton.cs
@@ -26,19 +26,19 @@ public class HoverButton : MonoBehaviour
         {
             // to next (right, down)
             // KEYBOARD
-            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) //keyboard
+            //if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) //keyboard
 
             // ALT CTRL
-            //if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.UpArrow)) //alt ctrl
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.UpArrow)) //alt ctrl
             {
                 ToNextButton();
             }
             // to previous (left, up)
             // KEYBOARD
-            else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.UpArrow)) //keyboard
+            //else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.UpArrow)) //keyboard
 
             //ALT CTRL
-            //else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) // alt ctrl
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) // alt ctrl
                 {
                 ToPreviousButton();
             }
